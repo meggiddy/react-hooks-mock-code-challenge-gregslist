0fd9fbab710116f8e5db0d82e15ae1c3fbdf3572
meggidy <mimamitchelle@gmail.com>
2023-02-01 10:17:20 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/listings")
+      .then((response) => response.json())
+      .then((data) => setListings(data));
+  }, []);
+  const handleDelete = (listingId) => {
+    const filteredList = listings.filter((listing) => {
+      if (listing.id !== listingId) {
+        return true;
+      }
+      return false;
+    });
+    setListings(filteredList);
+  };
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer handleDelete={handleDelete} listings={listings} />
     </div>
   );
 }
